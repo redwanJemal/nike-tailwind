c3e54bbdedbc20c2b8591ee54f64924a7692348f
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-08-31 16:08:24 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-4xl font-bold underline text-center text-green-600 uppercase">
+      Nike Primary set up done
+    </h1>
+  );
+}
+
+export default App;
