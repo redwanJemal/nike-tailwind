55bd34af7efea7926b880b23d4ddf4cc8cbee064
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-09-04 22:17:19 +0600

    superQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red border-coral-red rounded-full text-white">
       {label}
-      <img
-        className="ml-2 rounded-full w-5 h-5"
-        src={iconUrl}
-        alt="rightArrow"
-      />
+      {iconUrl && (
+        <img
+          className="ml-2 rounded-full w-5 h-5"
+          src={iconUrl}
+          alt="rightArrow"
+        />
+      )}
     </button>
   );
 };
