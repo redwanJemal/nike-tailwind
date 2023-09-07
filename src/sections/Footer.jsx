b673b81230fec485faa066bfe1b0e71355a60528
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-09-07 22:01:38 +0600

    footerSection

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,66 @@
+import { copyrightSign } from '../assets/icons';
+import { footerLogo } from '../assets/images';
+import { footerLinks, socialMedia } from '../constants';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="Nike logo" width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center mt-8 gap-5">
+            {socialMedia.map((icon) => (
+              <div
+                key={icon.alt}
+                className="flex justify-center items-center gap-6 w-12 h-12 bg-white rounded-full"
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-1  justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section.title}>
+              <h4 className="text-white font-montserrat text-2xl font-medium mb-6 leading-normal">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                    key={link.name}
+                  >
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="text-white-400 mt-24 flex justify-between  max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copyright sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
